47c60e66e6e59f74526a5c38cd286123f9a31070
Estevan Barbará <estevan.barbara@gmail.com>
2021-07-26 08:51:32 -0300

    Leitura e processamento de todos os arquivos da lista

--- src/App.java
+++ src/App.java
@@ -1,20 +1,61 @@
 import unirio.apa.Graph;
 import unirio.apa.util.FileManager;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
-    static String test1 = "input/test_set1/check_v5_s1.dat";
-    static String test2 = "input/test_set2/check_v5_s2.dat";
-    
+    private static final String TEST = "test";
+    private static final String INPUT = "input";
+    private static final String TEST_PATTERN = "check_v5_s";
+    private static final String TEST_EXT = ".dat";
+    private static final String MAIN_EXT = ".stp";
 
     public static void main(String[] args) {
-        System.out.println("Test instance 1");
-        Graph gTest1 = FileManager.loadTestFile(test1);
-        gTest1.printAdjacencyList();
+        // Checa se é um teste
+        boolean testRun = Arrays.stream(args).anyMatch(TEST::equals);
 
-        System.out.println("--------------------------------------------------------------");
+        // Lista todos os arquivos no diretório de INPUT
+        try(Stream<Path> walk = Files.walk(Paths.get(INPUT))){
+            List<String> files = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+            for(String filePath : files){
+                if(testRun){
+                    if(!filePath.contains(TEST_PATTERN)){
+                        continue;
+                    }
+                }
+                runDijkstra(filePath);
+                System.out.println("--------------------------------------------------------------");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
-        System.out.println("Test instance 2");
-        Graph gTest2 = FileManager.loadTestFile(test2);
-        gTest2.printAdjacencyList();
+    private static void runDijkstra(String filePath){
+        System.out.println(filePath);
+        Graph graph = null;
+
+        if(filePath.contains(TEST_EXT)){
+            graph = FileManager.loadTestFile(filePath);
+        }else if(filePath.contains(MAIN_EXT)) {
+            graph = FileManager.loadMainFile(filePath);
+        }else{
+            System.out.println("Tipo de arquivo não reconhecido");
+            return;
+        }
+        if(Objects.nonNull(graph)){
+            graph.printAdjacencyList();
+        }
     }
 }
